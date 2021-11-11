13b86a8cb954b5f641d3632488bef9831c7e8cf7
crisboxing11 <crisboxing11@gmail.com>
2021-11-10 20:27:13 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
