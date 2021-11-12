c770a52e77846950cb44e93903f28e4f73d50d62
crisboxing11 <crisboxing11@gmail.com>
2021-11-12 10:51:40 -0800

    Issues with authentication

--- src/firebase.js
+++ src/firebase.js
@@ -19,3 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth, analytics };
