f2e2668f18b7f3b80a9aa51fcb3bf47ef1c2192b
crisboxing11 <crisboxing11@gmail.com>
2021-11-11 22:49:10 -0800

    App updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import './App.css';
-import Header from './Header'
-import Home from './Home'
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Home from "./Home";
 function App() {
-  return (
-    <Router>
-    <div className="app">
-    <Header />
-      <Switch>
-        <Route path="/checkout">
-   
-      <Checkout />
-      </Route>
-      <Route exact path="/">
-        <Home />
-        </Route>
-      </Switch>
-    </div>
-    </Router>
-  );
-}
+  useEffect(() => {
+    // will only run once when the app component loads...
 
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+return (
+  <div className="app">
+    <Router>
+      <Switch>
+      <Route path="/login">
+         <Login />
+         </Route>
+      <Route path="/">
+        <Header />
+         <Home />
+         </Route>
+      </Switch>
+    </Router>
+  </div>
+);
+}
 export default App;
