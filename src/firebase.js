9a629c56bad52aae07bac69174319c0c76a1003b
crisboxing11 <crisboxing11@gmail.com>
2021-11-12 12:57:17 -0800

    checkoutproduct page having router issues

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD5hgQ7SE1ogQe_44uy9SZqvSpzsuFa2F0",
@@ -16,10 +16,9 @@ const firebaseConfig = {
   measurementId: "G-LGYYNFDLKH"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-export { db, auth, analytics };
+export { db, auth };
