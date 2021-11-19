ddcf6c653965d9e759a16af1c5fc0714f564702c
crisboxing11 <crisboxing11@gmail.com>
2021-11-18 20:18:03 -0800

    Added changes to basket

--- src/Product.js
+++ src/Product.js
@@ -38,7 +38,7 @@ function Product({ id, title, image, price, rating }) {
 
       <img src={image} alt="" />
 
-      <button onClick={addToBasket}>Add to Basket</button>
+      <button onClick={addToBasket}>{basket?.length}</button>
     </div>
   );
 }
